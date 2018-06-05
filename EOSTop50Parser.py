9424416416ee9a3b76ac5d058a00f60b8da040cd
Xiang Jiang <xiangj1@outlook.com>
2018-06-04 17:10:40 -0700

    changed to json format

--- EOSTop50Parser.py
+++ EOSTop50Parser.py
@@ -1,10 +1,13 @@
 from html.parser import HTMLParser
+import os
 
 class MyHTMLParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.round = 0
         self.isTd = 0
+        self.file = open("EOSdata.json", "w+")
+        self.file.write("[")
 
     def handle_starttag(self, tag, attrs):
         if(tag == 'td'):
@@ -13,15 +16,22 @@ class MyHTMLParser(HTMLParser):
     def handle_endtag(self, tag):
         if(tag == 'td'):
             self.isTd = 0
+        elif(tag == 'table'):
+            self.file.write("{\"rank\":\"eof\"}]")
 
     def handle_data(self, data):
         if(self.isTd == 1):
             self.round += 1
-            if(self.round < 4):
-                print(data, end=",")
-            else:
-                print(data, end="\n")
+            if(self.round == 1):
+                self.file.write("{\"rank\":\"" + data + "\",")
+            elif(self.round == 2):
+                self.file.write("\"adress\":\"" + data + "\",")
+            elif(self.round == 3):
+                self.file.write("\"amount\":\"" + data + "\",")
+            elif(self.round == 4):
+                self.file.write("\"percentage\":\"" + data + "\"},")
                 self.round = 0
+            
 
 parser = MyHTMLParser()
 
