58c187d72816a56c9c4a9e5dd4a650a066bc0d65
Xiang Jiang <xiangj1@outlook.com>
2018-06-23 19:19:51 -0700

    Rename Ethereum->Ethereum.py

--- Ethereum.py
+++ Ethereum.py
@@ -1,10 +1,7 @@
 from html.parser import HTMLParser
 from urllib.request import Request, urlopen
 import pandas as pd
-import numpy as np
 import time
-
-
 
 class wallet_info(HTMLParser):
     def __init__(self):
@@ -14,55 +11,57 @@ class wallet_info(HTMLParser):
         self.isTd = 0
 
     def handle_starttag(self, tag, attrs):
-        if(tag=='tbody'):
-            self.inTbody =1
+        if(tag == 'tbody'):
+            self.inTbody = 1
         if(tag == 'td' and self.inTbody == 1):
             self.isTd = 1
-        
+
     def handle_endtag(self, tag):
-        if(tag=='tbody'):
+        if(tag == 'tbody'):
             self.inTbody = 0
         if(tag == 'td'):
-            self.isTd = 0        
+            self.isTd = 0
 
     def handle_data(self, data):
         if(self.isTd == 1):
             self.round += 1
-            daf.append(data.replace(",","").replace(" Ether",""))
- 
+            daf.append(data.replace(",", "").replace(" Ether", ""))
+
+#global variable
 start = time.time()
 daf = []
 Rank = []
-Wallet_Address=[]
+Wallet_Address = []
 Quantity = []
 Percentage = []
-
 
 url = 'https://etherscan.io/accounts/1?ps=50'
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
 parser = wallet_info()
+
+
 parser.feed(str(webpage))
 j = 0
-for k in range(0,10000):
+for k in range(0, 10000):
     try:
         if(daf[j] == " "):
             del daf[j]
         elif('|' in daf[j]):
             del daf[j]
         elif(daf[j+1] == '.'):
-            daf[j] =str(daf[j])+str(daf[j+1])+str(daf[j+2])
+            daf[j] = str(daf[j])+str(daf[j+1])+str(daf[j+2])
             del daf[j+1]
             del daf[j+1]
         else:
-            j +=1
+            j += 1
     except IndexError:
         break
 
 for i in range(0, len(daf)):
     print(i, daf[i])
-    
-for l in range(0,100):
+
+for l in range(0, 100):
     try:
         Rank.append(daf[l*5])
         Wallet_Address.append(daf[l*5+1])
@@ -70,11 +69,11 @@ for l in range(0,100):
         Percentage.append(daf[l*5+3])
     except IndexError:
         break
-df = pd.DataFrame({'Rank':Rank,'Wallet_Address':Wallet_Address,'Quantity':Quantity,'Percentage':Percentage})
+df = pd.DataFrame({'Rank': Rank, 'Wallet_Address': Wallet_Address,
+                   'Quantity': Quantity, 'Percentage': Percentage})
 df.to_csv("token_wallet_Ethereum.csv")
 
 
-end = time.time()                 
+end = time.time()
 print("Time Used(in seconds):")
 print(end - start)
-
